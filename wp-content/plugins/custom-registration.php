56f9c4d7a6598c2dab4ad08155e4a800159a37bd
Marco d'Amore <mdamore9@gmail.com>
2017-08-05 21:39:48 +0000

    create user

---
+++ wp-content/plugins/custom-registration.php
@@ -0,0 +1,193 @@
+<?php
+
+/*
+  Plugin Name: Custom Registration
+  Description: Updates user rating based on number of posts.
+  Version: 1.2
+  Author: Tristan Slater w/ Agbonghama Collins
+  Author URI: http://kanso.ca
+ */
+
+/////////////////
+// PLUGIN CORE //
+/////////////////
+
+function cr(&$fields, &$errors) {
+  
+  // Check args and replace if necessary
+  if (!is_array($fields))     $fields = array();
+  if (!is_wp_error($errors))  $errors = new WP_Error;
+  
+  // Check for form submit
+  if (isset($_POST['submit'])) {
+    
+    // Get fields from submitted form
+    $fields = cr_get_fields();
+    
+    // Validate fields and produce errors
+    if (cr_validate($fields, $errors)) {
+
+      // If successful, register user
+      $user_id = wp_insert_user($fields);
+
+      insert_user_meta($user_id, $fields);
+
+      // And display a message
+      echo 'Registration complete. Goto <a href="' . get_site_url() . '/wp-login.php">login page</a>.';
+      
+      // Clear field data
+      $fields = array(); 
+    }
+  }
+  
+  // Santitize fields
+  cr_sanitize($fields);
+
+  // Generate form
+  cr_display_form($fields, $errors);
+}
+
+function cr_sanitize(&$fields) {
+  $fields['user_pass']    =  isset($fields['user_pass'])   ? esc_attr($fields['user_pass']) : '';
+  $fields['user_email']   =   isset($fields['user_email'])  ? sanitize_email($fields['user_email']) : '';
+  $fields['user_login']   = $fields['user_email'];
+  $fields['user_url']     =  isset($fields['user_url'])    ? esc_url($fields['user_url']) : '';
+  $fields['first_name']   =  isset($fields['first_name'])  ? sanitize_text_field($fields['first_name']) : '';
+  $fields['last_name']    =  isset($fields['last_name'])   ? sanitize_text_field($fields['last_name']) : '';
+  $fields['gender']       =   isset($fields['gender'])   ? sanitize_text_field($fields['gender']) : '';
+  $fields['birthday']     =   isset($fields['birthday'])   ? sanitize_text_field($fields['birthday']) : '';
+}
+
+function insert_user_meta($user_id, $fields) {
+  update_user_meta( $user_id, 'gender', $fields['gender'] );
+  update_user_meta( $user_id, 'birthday', $fields['birthday'] );
+  update_user_meta( $user_id, 'user_paid', '0');
+  update_user_meta( $user_id, 'waiver_signed', '0');
+}
+
+function cr_display_form($fields = array(), $errors = null) {
+  
+  // Check for wp error obj and see if it has any errors  
+  if (is_wp_error($errors) && count($errors->get_error_messages()) > 0) {
+    
+    // Display errors
+    ?><ul><?php
+    foreach ($errors->get_error_messages() as $key => $val) {
+      ?><li>
+        <?php echo $val; ?>
+      </li><?php
+    }
+    ?></ul><?php
+  }
+  
+  // Disaply form
+  
+  ?><form action="<?php $_SERVER['REQUEST_URI'] ?>" method="post">
+
+    <div>
+      <label for="email">Email <strong>*</strong></label>
+      <input type="text" name="user_email" value="<?php echo (isset($fields['user_email']) ? $fields['user_email'] : '') ?>">
+    </div>
+
+    <div>
+      <label for="user_pass">Password <strong>*</strong></label>
+      <input type="password" name="user_pass">
+    </div>
+  
+    <div>
+      <label for="firstname">First Name</label>
+      <input type="text" name="first_name" value="<?php echo (isset($fields['first_name']) ? $fields['first_name'] : '') ?>">
+    </div>
+    
+    <div>
+      <label for="lastname">Last Name</label>
+      <input type="text" name="last_name" value="<?php echo (isset($fields['last_name']) ? $fields['last_name'] : '') ?>">
+    </div>
+
+    <div class="two_input_section">
+         <div class="input-title">Birthday</div>
+         <input class="first_input" type="text" id="date" name="birthday" required><br>
+    </div>
+
+    <div class="two_input_section">
+      <div class="input-title">Gender</div>
+        <select name="gender" required>
+          <option value="female">Female</option>
+          <option value="male">Male</option>
+        </select>
+        <br>
+    </div>
+
+            
+    <input type="submit" name="submit" value="Register">
+    </form><?php
+}
+
+function cr_get_fields() {
+  $user_email = isset($_POST['user_email'])   ?  $_POST['user_email']        :  '';
+  return array(
+    'user_pass'    =>  isset($_POST['user_pass'])    ?  $_POST['user_pass']    :  '',
+    'user_email'   =>  $user_email,
+    'user_login'   => $user_email,
+    'user_url'     =>  isset($_POST['user_url'])     ?  $_POST['user_url']     :  '',
+    'first_name'   =>  isset($_POST['first_name'])   ?  $_POST['first_name']        :  '',
+    'last_name'    =>  isset($_POST['last_name'])    ?  $_POST['last_name']        :  '',
+    'gender'       => isset($_POST['gender']) ? $_POST['gender'] : '',
+    'birthday'       => isset($_POST['birthday']) ? $_POST['birthday'] : '',
+  );
+}
+
+function cr_validate(&$fields, &$errors) {
+  
+  // Make sure there is a proper wp error obj
+  // If not, make one
+  if (!is_wp_error($errors))  $errors = new WP_Error;
+  
+  // Validate form data
+  
+  if (empty($fields['user_pass']) || empty($fields['user_email'])) {
+    $errors->add('field', 'Required form field is missing');
+  }
+
+  if (strlen($fields['user_pass']) < 5) {
+    $errors->add('user_pass', 'Password length must be greater than 5');
+  }
+
+  if (!is_email($fields['user_email'])) {
+    $errors->add('email_invalid', 'Email is not valid');
+  }
+
+  if (email_exists($fields['user_email'])) {
+    $errors->add('email', 'Email Already in use');
+  }
+  
+  // If errors were produced, fail
+  if (count($errors->get_error_messages()) > 0) {
+    return false;
+  }
+  
+  // Else, success!
+  return true;
+}
+
+
+
+///////////////
+// SHORTCODE //
+///////////////
+
+// The callback function for the [cr] shortcode
+function cr_cb() {
+  $fields = array();
+  $errors = new WP_Error();
+  
+  // Buffer output
+  ob_start();
+  
+  // Custom registration, go!
+  cr($fields, $errors);
+  
+  // Return buffer
+  return ob_get_clean();
+}
+add_shortcode('cr', 'cr_cb');
--- wp-content/themes/starkers-html5-master/page-sign-up.php
+++ wp-content/themes/starkers-html5-master/page-sign-up.php
@@ -10,64 +10,10 @@
 get_header();?>
 
 <div class="sign-up-bg">
-  <div class="sign-up-header-wrapper">
-<<<<<<< HEAD
-    <img class="home-arrows" src="/outdoors/wp-content/uploads/arrow-left.png" /><div class="sign-up-header">Join Outdoors</div><img class="home-arrows" src="/outdoors/wp-content/uploads/arrow-right.png" />
-  </div>
-  <div class="form-holder"><?php echo do_shortcode("[RM_Form id='3']"); ?></div>
-</div>
-
-
-
-
-<script>
-  var select = document.getElementsByName("Select_21")[0]
-=======
-    <img class="home-arrows" src="/outdoors/wp-content/uploads/arrow-left.png" /><div class="sign-up-header">Join Outdoors</div><img class="home-arrows" src="/outdoors/wp-content/uploads/arrow-right.png" /><br></br>
-  </div>
-  <!--<div class="form-holder"><?php echo do_shortcode("[RM_Form id='3']"); ?></div>-->
-
-<div class="form-wrapper">
-<form class="sign-up-form" method="post" action="http://localhost/outdoors/index.php/submit-sign-up">
-<div class="two_input_section">
-      <div class="input-title">First Name</div>
-      <input class="first_input" type="text" name="first_name" required><br>
-    </div><div class="two_input_section">
-      <div class="input-title">Last Name</div>
-      <input type="text" name="last_name" required><br>
-    </div>
-
-<div class="two_input_section">
-      <div class="input-title">Email</div>
-      <input class="first_input" type="text" name="email" required><br>
-    </div><div class="two_input_section">
-      <div class="input-title">Phone</div>
-      <input type="text" name="phone" required><br>
-    </div>
-
- <div class="two_input_section">
-      <div class="input-title">Birthday</div>
-      <input class="first_input" type="text" id="date" name="birthday" required><br>
-      </div><div class="two_input_section">
-      <div class="input-title">Gender</div>
-      <select name="gender" required>
-        <option value="volvo">Female</option>
-  		<option value="saab">Male</option></select><br>
-    </div>
-
-<!--
-Figure out drop down menu for Gender
--->
-
-<div class="two_input_section">
-      <div class="input-title">Password</div>
-      <input class="first_input" type="text" name="password" required><br>
-    </div><div class="two_input_section">
-      <div class="input-title">Confirm Password</div>
-      <input type="text" name="confirm_password" required><br>
-    </div>
-   <input type="submit" value="Submit">
-</form>
+	<div class="sign-up-header-wrapper">
+		<img class="home-arrows" src="/outdoors/wp-content/uploads/arrow-left.png" /><div class="sign-up-header">Join Outdoors</div><img class="home-arrows" src="/outdoors/wp-content/uploads/arrow-right.png" />
+	</div>
+	<?php echo do_shortcode("[cr]"); ?>
 </div>
 
 <link rel="stylesheet" href="https://unpkg.com/flatpickr/dist/flatpickr.min.css">
@@ -75,22 +21,16 @@ Figure out drop down menu for Gender
 
 
 <script>
-  flatpickr("#date", {altInput: true});
+	flatpickr("#date", {altInput: true});
 
-  <?php
+	<?php
+	?>
 
-  ?>
-
-  /**var select = document.getElementsByName("Select_21")[0]
->>>>>>> a8f942f211898a50cb3660e39caa7057dec37f5f
-  select.firstChild.innerHTML = "Gender";
-  select.firstChild.disabled = true;
-  document.getElementsByName("password")[0].placeholder = "Password";
-  document.getElementsByName("password_confirmation")[0].placeholder = "Confirm Password";
-<<<<<<< HEAD
-=======
-  */
->>>>>>> a8f942f211898a50cb3660e39caa7057dec37f5f
+	var select = document.getElementsByName("Select_21")[0]
+	select.firstChild.innerHTML = "Gender";
+	select.firstChild.disabled = true;
+	document.getElementsByName("password")[0].placeholder = "Password";
+	document.getElementsByName("password_confirmation")[0].placeholder = "Confirm Password";
 </script>
 
 <?php get_footer(); ?>
