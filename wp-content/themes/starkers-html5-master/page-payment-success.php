59c2510cdd06a964b7809d12e7ebf2b0b54bad69
Marco d'Amore <mdamore9@gmail.com>
2017-08-12 23:56:19 +0000

    added placeholder for payment  success page

---
+++ wp-content/themes/starkers-html5-master/page-payment-success.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * The main template file.
+ *
+ * @package WordPress
+ * @subpackage Starkers
+ * @since Starkers HTML5 3.0
+ */
+ 
+get_header();?>
+
+<div class="sign-up-bg">
+  <div class="sign-up-header-wrapper">
+    <img class="home-arrows" src="/outdoors/wp-content/uploads/arrow-left.png" /><div class="sign-up-header">Welcome!</div><img class="home-arrows" src="/outdoors/wp-content/uploads/arrow-right.png" />
+  </div>
+</div>
+
+<link rel="stylesheet" href="https://unpkg.com/flatpickr/dist/flatpickr.min.css">
+<script src="https://unpkg.com/flatpickr"></script>
+
+<?php get_footer(); ?>
